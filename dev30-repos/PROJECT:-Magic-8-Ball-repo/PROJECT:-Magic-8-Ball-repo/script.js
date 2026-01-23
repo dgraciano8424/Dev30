f54c90e723e3d02101dc098d0988b81e524be546
David <176092646+dgracianoBVT2024@users.noreply.github.com>
2026-01-23 08:14:14 -0800

    now final

--- dev30-repos/PROJECT:-Magic-8-Ball-repo/PROJECT:-Magic-8-Ball-repo/script.js
+++ dev30-repos/PROJECT:-Magic-8-Ball-repo/PROJECT:-Magic-8-Ball-repo/script.js
@@ -1,14 +1,14 @@
-const userName = "";
-// const greeting = 'Hello Jane!';
-// greeting: string ? `Hello ${userName}` : 'Hello!';
+const userName = "David";
+console.log(String ? `Hello ${userName}!` : "Hello!");
 
 //constant variable that prints out the question from User
 const userQuestion = "Will I ever win the lottery?";
-console.log(userQuestion);
+console.log(`${userName} asked, "${userQuestion}`);
 
 //constant variable that has the value of 2 built-in methods: Math.floor and Math.random. Multiplying random by any whole number gives (n - 1). I.E. If we used 1000 i'd get 999 different choices or numbers.
 const randomNumber = Math.floor(Math.random() * 8);
 const eightBall = randomNumber;
+
 //a switch statement taking the randomized number from variable "eightball" and using it to run the predetermined console.log depending on the RNG.
 switch (eightBall) {
   case 0:
