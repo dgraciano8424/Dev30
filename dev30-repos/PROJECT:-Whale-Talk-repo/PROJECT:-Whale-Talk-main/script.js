f652bc315240c3d0286119b19e830b4f4056b1e1
David <176092646+dgracianoBVT2024@users.noreply.github.com>
2026-01-31 19:05:27 -0800

    finalized Whale Talk project

--- dev30-repos/PROJECT:-Whale-Talk-repo/PROJECT:-Whale-Talk-main/script.js
+++ dev30-repos/PROJECT:-Whale-Talk-repo/PROJECT:-Whale-Talk-main/script.js
@@ -1,5 +1,5 @@
 //Step 1
-var input = "P. Sherman, 42 Wallaby Way, Sydney";
+var input = "P. Sherman 42 Wallaby Way Sydney";
 
 //Step 2
 const vowels = ["a", "e", "i", "o", "u"];
@@ -8,7 +8,39 @@ const vowels = ["a", "e", "i", "o", "u"];
 var resultArray = [];
 
 //Step 4
-for (let input = 0; input < array.length; input++) {
-  const resultArray = array[input];
-  console.log(resultArray);
+//For loop that iterates through a string.
+for (let i = 0; i < input.length; i++) {
+  // var resultArray = input[i];
+
+  // console.log(resultArray);
+  //Step 5
+  //console logging the "iterator" numbered position.
+  // console.log(i);
+
+  //Step 10 / 11
+  //Step 6
+  //Using a for loop that is nested inside another for loop that console logs (iterates) through all the vowels every time the outer loop is ran.
+  for (let j = 0; j < vowels.length; j++) {
+    // console.log(vowels[j]);
+
+    //Step 7
+    //Console.log'd iterator number from this inner for loop
+    // console.log([j]);
+
+    //Step 8 Inside the second for loop is a conditional that will print the matching letters, how many appearances and mark them "True".
+    if (vowels[j] === input[i]) {
+      // console.log(input[i], "True");
+      //Step 9  Now instead of just logging the letters, add them to the results array.
+      resultArray.push(vowels[j]);
+      if (input[i] === "e" || input[i] === "u") {
+        resultArray.push(input[i]);
+      }
+    }
+  }
 }
+//Step 9.2 Console log'd resultArray to check if working
+//Step 12 as well.
+// console.log(resultArray);
+var resultString = resultArray.join("");
+resultString = resultString.toUpperCase();
+console.log(resultString);
